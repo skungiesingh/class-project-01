2e6f98aff19db480e50f7801ca15a2516f7b29c7
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-04-06 11:05:32 +0300

    cleaned code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,8 @@ var forecastBoxEl = document.querySelector("#forecast_box")
 var cityInputEl = document.querySelector("#cityInput")
 var userFormEl = document.querySelector("#user-form")
 var searchHistoryEl = document.querySelector("#search-history")
-var cityNameHolder = []
-
-
-
-
-// init cityNameArray
-var cityNameArray = JSON.parse(localStorage.getItem("cityNameArray"));
-   // //init homepage if none exist in local storage
-   if(!cityNameArray){
-       cityNameArray = ["Miami", "Tampa", "Jacksonville", "Melbourne"]
-    
-   } else {
-       getCityInfo(cityNameArray[cityNameArray.length-1])
-       searchHistoryBtns();
-  }
+var cityNameArray = ["Miami", "Tampa", "Jacksonville", "Melbourne"]
+   
 
 //time converter (time is received from api as a unix code)
 function timeConverter (inputTime) {
@@ -50,23 +37,16 @@ function getCityInfo (city) {
     .then(function(data){
         var cityLat = data.coord.lat;
         var cityLon = data.coord.lon;
-        var requestName = data.name;
+        
         
         //send info
-        cityUvInfo (cityLat, cityLon, requestName);
-        
-        //to get the same name regardless of user input
-        localStorageHolder(requestName);
-        cityNameHolder = requestName;
+        cityUvInfo (cityLat, cityLon);
+       
     });
 };
 
-
-
-
-
 //main content builder
-async function cityUvInfo (lat, lon, requestName) {
+async function cityUvInfo (lat, lon) {
     //format request to url
     // debugger;
 
@@ -74,6 +54,8 @@ async function cityUvInfo (lat, lon, requestName) {
     var oneUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +  "&appid=27a6e74d4260774945191a8dc4b750e0&units=imperial"
     var stormApi = "https://api.stormglass.io/v2/tide/extremes/point?lat=" + lat + "&lng=" + lon;
 
+
+    //tide api fetcher to make available within the cityUVInfot
     var stormApiArr = await fetch(stormApi, {
         headers: {
             'Authorization': 'a6c43a5a-9523-11eb-a242-0242ac130002-a6c43adc-9523-11eb-a242-0242ac130002'
@@ -83,12 +65,10 @@ async function cityUvInfo (lat, lon, requestName) {
             return jsonData.json();
         })
         .then(function(jsonData){
-           
-            console.log(jsonData)
             return jsonData;
         });
 
-        console.log(stormApiArr)
+      
 
     //make request to api
     fetch(oneUrl).then(function(response){
@@ -106,16 +86,15 @@ async function cityUvInfo (lat, lon, requestName) {
                 Icon: "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png"
             
             }
-            
-            
 
+            //increment counters 
             var a = i*4;
             var b = a+1;
             var c = a+2;
             var d = a+3;
              
-
-            function tideBuilder(j){
+            //nested function get the correct format from the api date
+            function tideTimeBuilder(j){
                 var experiment = stormApiArr.data[j].time
                 var expPart2 = experiment.split("T")
                 var expPart3 = expPart2[1].split("+")
@@ -123,20 +102,22 @@ async function cityUvInfo (lat, lon, requestName) {
                 var expPart5 = expPart4[0] + ":" + expPart4[1];
                 return expPart5
             }
+
+            // tide object
             var tide ={
-                periodOneTime: tideBuilder(a),
+                periodOneTime: tideTimeBuilder(a),
                 periodOneHeight: stormApiArr.data[a].height.toFixed(2),
                 periodOneType: stormApiArr.data[a].type,
 
-                periodTwoTime: tideBuilder(b),
+                periodTwoTime: tideTimeBuilder(b),
                 periodTwoHeight: stormApiArr.data[b].height.toFixed(2),
                 periodTwoType: stormApiArr.data[b].type,
 
-                periodThreeTime: tideBuilder(c),
+                periodThreeTime: tideTimeBuilder(c),
                 periodThreeHeight: stormApiArr.data[c].height.toFixed(2),
                 periodThreeType: stormApiArr.data[c].type,
 
-                periodFourTime: tideBuilder(d),
+                periodFourTime: tideTimeBuilder(d),
                 periodFourHeight: stormApiArr.data[d].height.toFixed(2),
                 periodFourType: stormApiArr.data[d].type
 
@@ -196,52 +177,6 @@ async function cityUvInfo (lat, lon, requestName) {
     });
 }
 
-//user input handler
-function formSubmitHandler (event) {
-    event.preventDefault();
-
-    var cityContent = cityInputEl.value.trim();
-
-    if (cityContent){
-        getCityInfo(cityContent);
-
-        //clear old content
-        contentBoxEl.textContent="";
-        forecastBoxEl.textContent= "";
-        cityInputEl.value= "";
-    } else {
-        alert("Please Enter City Name")
-    }
-}
-
-//storage handler
-function localStorageHolder (cityName) {
-    var nameCount = 0;
-    
-    //checks the previous 10 names
-    if (cityNameArray.length <= 10) {
-        for (i=cityNameArray.length-1; i>=0 ; i--){
-            if (cityName === cityNameArray[i]){
-                nameCount++;
-            }
-        }
-    } else {
-        //checks previous 10 names if array is over 10
-        for (i=cityNameArray.length-1; i>cityNameArray.length-10 ; i--){
-            if (cityName === cityNameArray[i]){
-                nameCount++;
-            }
-        }
-    }     
-
-    //ensures cityNameArray doesn't hold duplicate names with range of 10 inputs
-    if (nameCount === 0){
-        cityNameArray.push(cityName);
-        localStorage.setItem("cityNameArray", JSON.stringify(cityNameArray))
-        searchHistoryBtns();
-    }
-}
-
 //search history button 
 function clickButtonHandler (event){
     var city = event.target.getAttribute("data-name");
@@ -256,34 +191,7 @@ function clickButtonHandler (event){
   }
 }
 
-//build search buttons
-function searchHistoryBtns (){
-        
-        searchHistoryEl.textContent=""
-
-        //builds max of 10 history buttons
-        if (cityNameArray.length <= 10) {
-            for (i=cityNameArray.length-1; i>=0 ; i--){
-                var searchHistoryBtnEl = document.createElement("button")
-                searchHistoryBtnEl.className = "names"
-                searchHistoryBtnEl.setAttribute("data-name", cityNameArray[i])
-                searchHistoryBtnEl.textContent = cityNameArray[i]
-
-                searchHistoryEl.appendChild(searchHistoryBtnEl)
-            }
-        } else {
-            //builds of of the last 10 user inputs
-            for (i=cityNameArray.length-1; i>cityNameArray.length-10 ; i--){
-                var searchHistoryBtnEl = document.createElement("button")
-                searchHistoryBtnEl.className = "names"
-                searchHistoryBtnEl.setAttribute("data-name", cityNameArray[i])
-                searchHistoryBtnEl.textContent = cityNameArray[i]
-
-                searchHistoryEl.appendChild(searchHistoryBtnEl)
-            }
-        }        
-}
 
 //event listeners
 searchHistoryEl.addEventListener("click", clickButtonHandler)
-userFormEl.addEventListener("submit", formSubmitHandler);
+
